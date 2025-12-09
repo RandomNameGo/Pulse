a205832447976304bf97d68f392c07c9c84bf178
Admin <khangdoan20122004@gmail.com>
2025-12-09 22:39:33 +0700

    fix: OpenApiConfig

--- src/main/java/com/app/pulse/configurations/OpenApiConfig.java
+++ src/main/java/com/app/pulse/configurations/OpenApiConfig.java
@@ -5,14 +5,20 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://pleasant-grace-production.up.railway.app")
+                ))
                 .info(new Info()
                         .title("Pulse API Documentation")
                         .description("API documentation for the Pulse system.")
