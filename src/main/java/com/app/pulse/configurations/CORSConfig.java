a9dd2b489c89bc8c62713a76366dd2fb0411e1f3
Admin <khangdoan20122004@gmail.com>
2025-12-09 22:04:49 +0700

    fix: add CORS config

--- src/main/java/com/app/pulse/configurations/CORSConfig.java
+++ src/main/java/com/app/pulse/configurations/CORSConfig.java
@@ -13,7 +13,7 @@ public class CORSConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .exposedHeaders("*")
